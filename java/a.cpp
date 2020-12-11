489acea0dc6b6a88561022f84b9e3efd8c5f6786
siddharth singh <you@example.com>
2020-12-11 18:00:46 +0530

    dht

---
+++ java/a.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll int64_t
+#include <cstdlib> 
+#include <math.h>
+#include<cstdio>
+#include<cstring>
+#include <math.h>
+#define vi v(n) vector<ll> v(n);
+#define FOR(I, A, B) for (ll I = (A); I <= (B); I++)
+#define fo(i,n) for(ll i=0;i<n;i++)
+#define sz(a) ll((a).size())
+#define pb push_back
+#define all(c) (c).begin(),(c).end()
+#define dbg(x) cout << #x << " = " << x << endl
+#define dbg2(x,y) cout << #x << " = " << x << ", " << #y << " = " << y << endl
+#define dbg3(x,y,z) cout << #x << " = " << x << ", " << #y << " = " << y << ", " << #z << " = " << z << endl
+#define dbg4(x,y,z,q) cout << #x << " = " << x << ", " << #y << " = " << y << ", " << #z << " = " << z << ", " << #q << " = " << q << endl
+#define scan(char_array) scanf("%[^\n]s",&char_array)
+#define IOS ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+
+int main(){
+    set<char> s;
+    
+}
